691eb0e9bc57a3c2382f2c1de153e418cbcaff74
Nhan <nktran4@gmail.com>
2022-05-20 17:58:50 -0400

    appointment apply to application

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import Button from "components/Button"  
+import InterviewerList from "components/InterviewerList"  
+
+
+export default function Form(props) {
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = () => {
+    setStudent("")
+    setInterviewer(null)
+  }
+
+  const cancel = () => {
+    reset()
+    props.onCancel()
+  }
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form onSubmit={event => event.preventDefault()}>
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            onChange={(e) => setStudent(e.target.value)}
+            interviewer={props.interviewer}
+            interviewers={props.interviewers}
+            onSave={props.onSave}
+            onCancel={props.onCancel}
+          />
+        </form>
+        <InterviewerList 
+          interviewers = {props.interviewers}
+          value = {props.interviewer}
+          onChange={setInterviewer}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={props.onSave}>Save</Button>
+        </section>
+      </section>
+    </main>
+  );
+}
